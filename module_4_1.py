0af6916b461bd08983c840ac566874696cc65005
Den-VB <print.denvb@gmail.com>
2024-10-27 14:48:02 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,9 @@
+# Задача "А как делить?"/data
+import module_4_1_fake_math as f_m
+import module_4_1_true_math as t_m
+f_m.divide(69, 3)
+f_m.divide(3, 0)
+t_m.divide(49, 7)
+t_m.divide(15, 0)
+t_m.divide(-15, 0)
+
